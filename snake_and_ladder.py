bc8ba3d70573d2168bfb64cce0f3772c9d790ef4
Kplasma <3624prajubhaigaming@gmail.com>
2024-11-10 18:31:20 +0530

    Update snake_and_ladder.py
    # Snake and Ladder Game
    This is a simple Snake and Ladder game implemented in Python using loops and the random module.
    
    ## How to Play
    - Run the `snake_and_ladder.py` script.
    - Follow the prompts to roll the dice.
    - Reach position 100 to win!

--- snake_and_ladder.py
+++ snake_and_ladder.py
@@ -5,16 +5,20 @@ def roll_dice(sides=6):
 snakes={38: 20, 45: 20, 51: 20, 65: 20, 91: 20, 97: 20}
 
 ladders={32: 50, 40: 50, 56: 50, 17: 50}
+#Initialize player's position 
 position=1
 print(f' position ={position}' , '\n')
+#Game loop
 while True :
+    #Ask the player if they want to play
     status = input('wanna play ? , type no if u dont : \n ')
     if status=='no':
         print(f' position ={position}' , '\n')
         print(' Bye then ')
-        break     
+        break
+    #Roll the dice    
     step=roll_dice()
-    
+    #check if the new position is in snake or ladder 
     if position+step in snakes :
         position =snakes[position+step]
     elif position+step in ladders:
